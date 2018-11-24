c07eedb32f2a4b5e8d423114deb50f29c3a5dd32
Maciekxdabu <40292375+Maciekxdabu@users.noreply.github.com>
2018-11-24 17:22:13 +0100

    Tworze okienko

--- main.cpp
+++ main.cpp
@@ -5,5 +5,21 @@ using namespace std;
 
 int main()
 {
+    sf::RenderWindow okno(sf::VideoMode(500, 500, 32), "Projekt Edukacja");
+
+    while (okno.isOpen())
+    {
+        sf::Event zdarzenie;
+        while (okno.pollEvent(zdarzenie))
+        {
+            if (zdarzenie.type == sf::Event::Closed)
+                okno.close();
+        }
+
+        okno.clear(sf::Color::White);
+
+        okno.display();
+    }
+
     return 0;
 }
