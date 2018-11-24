7a3ca68dce23b141cfd512d6e7f9c129e7c3522b
Maciekxdabu <40292375+Maciekxdabu@users.noreply.github.com>
2018-11-24 22:25:21 +0100

    Update klasy.h

--- klasy.h
+++ klasy.h
@@ -14,7 +14,18 @@ public:
 
 class Gracz
 {
-    ;
+protected:
+    sf::Sprite obraz;
+    sf::Texture tekstura;
+    sf::Vector2f przyspieszenie;
+
+public:
+    Gracz(std::string);
+    ~Gracz();
+    sf::Sprite getObraz();
+    void ruch(float, float);
+    void drag();
+    void porusz();
 };
 
 class Wskaznik
