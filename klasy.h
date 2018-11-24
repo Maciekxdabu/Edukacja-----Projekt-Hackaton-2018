c200daf0a04d3a6642f9adb55009e6aa1b840103
xDiodakx <40292335+xDiodakx@users.noreply.github.com>
2018-11-24 22:24:34 +0100

    aldasldlas

--- klasy.h
+++ klasy.h
@@ -19,10 +19,27 @@ class Gracz
 
 class Wskaznik
 {
+    protected:
 
+        sf::Sprite celownik;
+        sf::Texture tekstura;
+
+    public:
+        Wskaznik(std::string);
+        ~Wskaznik ();
+        sf::Sprite getObraz();
+        void setposition(float,float);
 };
 
 class Wrog
+{
+    protected:
+
+        sf::Sprite
+
+};
+
+class Przeszkoda
 {
     ;
 };
